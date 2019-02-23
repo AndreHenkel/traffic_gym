bc1b109f56971569f7becb60b937897ce5d3e3bf
ajohe <ajohe@web.de>
2019-02-23 14:48:37 +0100

    feat: Script to start simulation

---
+++ gym_traffic/envs/start_sim.py
@@ -0,0 +1,18 @@
+from controller import Controller
+import arcade
+from display import Display
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 800
+
+def main():
+    
+    cnt = Controller(SCREEN_WIDTH, SCREEN_HEIGHT)
+    display = Display(cnt)
+    display.setup()
+    arcade.run()
+    
+
+
+if __name__ == "__main__":
+    main()
