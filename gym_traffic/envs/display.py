1f2b01675f332cb9a16d5fd8d291c9b0b97ccbbb
ajohe <ajohe@web.de>
2019-12-24 13:45:17 +0100

    feat: Added desktop icon for program
    Currently only using the scaled down version of the standard car vehicle

--- gym_traffic/envs/display.py
+++ gym_traffic/envs/display.py
@@ -4,6 +4,7 @@ This class will give the user the possibility to watch the traffic with a graphi
 from PIL import Image
 import arcade
 import numpy as np
+import os
 import random
 import pyglet
 from pyglet import clock
@@ -22,13 +23,15 @@ class Display(pyglet.window.Window):
         """
             expects streets, as in Street class
             and vehicles as in Vehicle class
-            
+
             they will be transformed then to sprites and displayed
-            
+
         """
         super(Display, self).__init__(cnt.width,cnt.height, resizable=False, fullscreen=False, caption="Display")
         self.cnt = cnt
-        
+        directory = os.path.dirname(os.path.realpath(__file__))
+        self.set_icon(pyglet.image.load(directory+"/img/car_w_rights.png"))
+
 
     def setup(self):
         # Set up your game here
@@ -53,7 +56,7 @@ class Display(pyglet.window.Window):
                     arcade.draw_circle_outline(tl.pos["x"], tl.pos["y"], 4, arcade.color.GREEN, 3)
                 else:
                     arcade.draw_circle_outline(tl.pos["x"], tl.pos["y"], 4, arcade.color.RED, 3)
-        
+
         # draw all vehicles
         self.veh_draw()
         # render
@@ -71,8 +74,8 @@ class Display(pyglet.window.Window):
             self.close()
 
     def update(self, delta_time):
-        """ All the logic to move, and the game logic goes here. 
-            For an first easy implementation, all vehicles from control will be sent here and drawn as sprites.  
+        """ All the logic to move, and the game logic goes here.
+            For an first easy implementation, all vehicles from control will be sent here and drawn as sprites.
             Those sprites will be identified through the unique "registration plate number" on each vehicle.
         """
         self.cnt.step(delta_time)
@@ -81,7 +84,7 @@ class Display(pyglet.window.Window):
         """
             Initializes data with the appropriate size of the screen, taken by width and height.
             Fill the data buffer with 3 unsigned bytes per pixel(rgb), then returns it as numpy array.
-            
+
             @return: numpy array of screen as RGB format
         """
         data = ( GLubyte * (3*self.cnt.width*self.cnt.height) )(0)
@@ -89,4 +92,3 @@ class Display(pyglet.window.Window):
         arr = np.frombuffer(bytearray(data),dtype=np.uint8)
         arr = arr.reshape(self.cnt.width, self.cnt.height,3)
         return np.flipud(arr)
-
